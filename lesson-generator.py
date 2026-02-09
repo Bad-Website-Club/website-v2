b9ac3b4c5095a7a98cae6395f93c615256753372
rivea0 <58330360+rivea0@users.noreply.github.com>
2026-02-09 13:06:22 +0300

    Add frontmatter

--- lesson-generator.py
+++ lesson-generator.py
@@ -1,4 +1,5 @@
 import argparse
+from datetime import datetime
 import pandas
 
 def main():
@@ -14,6 +15,17 @@ def generate_lesson_from_row(row, bootcamp):
     filename = f'./content/bootcamps/{bootcamp}/lessons/{row["Lesson number"]}.md'
     with open(filename, 'w+', encoding='utf-8') as f:
         f.write('+++\n')
+        f.write(f'date = \'{datetime.now().astimezone()}\'\n')
+        f.write(f'etz_url = \'\'\n')
+        f.write(f'draft = true\n')
+        f.write(f'title = \'{row["Name"]}\'\n')
+        f.write(f'youtube_id = \'\'\n')
+        f.write(f'alternative_recording_urls = []\n')
+        f.write(f'fcc_lesson_url = \'{row["Link"]}\'\n')
+        f.write(f'type = \'lessons\'\n')
+        f.write(f'lesson_type = [\'{row["Lesson type"]}\']\n')
+        f.write(f'instructors = [\'Jess\', \'Carmen\', \'Eda\']\n')
+        f.write(f'unit = [\'{row["Unit"]}\']\n')
         f.write('+++\n')
 
 
