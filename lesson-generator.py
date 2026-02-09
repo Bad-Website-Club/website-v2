0e912c1967c4b38ef330e4ade73a003a2376ad5f
rivea0 <58330360+rivea0@users.noreply.github.com>
2026-02-09 12:41:20 +0300

    Start generating markdown from row

--- lesson-generator.py
+++ lesson-generator.py
@@ -4,11 +4,17 @@ import pandas
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('file', help='Path to the CSV file', type=str)
+    parser.add_argument('bootcamp', help='The bootcamp name', type=str)
     args = parser.parse_args()
     csv = pandas.read_csv(args.file)
     first = csv.iloc[0].to_dict()
-    print(first)
+    generate_lesson_from_row(first, args.bootcamp)
 
+def generate_lesson_from_row(row, bootcamp):
+    filename = f'./content/bootcamps/{bootcamp}/lessons/{row["Lesson number"]}.md'
+    with open(filename, 'w+', encoding='utf-8') as f:
+        f.write('+++\n')
+        f.write('+++\n')
 
 
 if __name__ == '__main__':
